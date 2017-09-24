513ea094a00f1b79fe78c33a1c625d596c42277c
selvaganeshr <selvaganeshrajam@gmail.com>
2017-09-24 16:05:02 +0530

    iframe component added

---
+++ src/components/IframeComponent.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import PropTypes from "prop-types";
+
+class IFrameComponent extends PureComponent {
+
+  render() {    
+    return React.createElement("iframe",{
+        ref: "iframe",
+        src: this.props.url,
+        style: Object.assign({}, {
+        border:"0",
+          position: this.props.position || "absolute",
+          display: this.props.display || "block",
+          height: this.props.height || "100%",
+          width: this.props.width || "0px"
+        }, this.props.styles || {}),
+        height: this.props.height || "100%",
+        width: this.props.width || "0px",
+    })
+  }
+}
+
+IFrameComponent.propTypes = {
+    url: PropTypes.string.isRequired,
+    width: PropTypes.string,
+    position: PropTypes.string,
+    display: PropTypes.string,
+    height: PropTypes.string,
+    styles: PropTypes.object,
+    allowFullScreen: PropTypes.bool,
+  }
+
+export default IFrameComponent;
