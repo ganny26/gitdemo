25bfcabb455f57b69f3aa8ab53e92f5a443d4e72
selvaganeshr <selvaganeshrajam@gmail.com>
2017-09-24 16:04:44 +0530

    navigation bar added

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import 'typeface-roboto';
+import Player from './Player';
+import '../styles/reader.css';
+import '../styles/index.css';
+class NavigationBar extends Component {
+  render() {
+    return (
+ <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">
+  <span className="icon-WA2"/>
+  <span className="icon-cook_wa2cook"/>
+  </a>
+  <div className="playerContent">
+  <Player/>
+      </div>
+    </nav>
+
+    );
+  }
+}
+
+export default NavigationBar;
