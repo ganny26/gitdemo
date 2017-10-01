b017d3169adfaea7376749f84e21107267779c09
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-01 21:51:53 +0530

    player position organized

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,38 +4,25 @@ import Icon from 'material-ui/Icon';
 import IconManager from './IconManager';
 import Dropdown from './Dropdown';
 class Player extends Component {
-  render() {
-    return (
-        <div>
-            <div className="dropdown">
-                    <button className="btn languagebtn btn-secondary dropdown-toggle languagedd" type="button" id="ddLanguageBtn" data-toggle="dropdown">
-                English (US)
-            </button>
-            <div className="dropdown-menu" aria-labelledby="ddLanguageBtn">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
+    render() {
+        let languageList = ['English(IND)', 'English (US)', 'English(UK)'];
+        let speedList = ['Low', 'Medium', 'High'];
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <Dropdown value={languageList} name='Select Language' />
+                </div>
+                <div className="col-md-4 player-control">
+                    <Icon color="primary" style={{ fontSize: 55, cursor: 'pointer' }}>play_arrow</Icon>
+                    <Icon color="primary" style={{ fontSize: 55, cursor: 'pointer' }}>pause</Icon>
+                    <Icon color="primary" style={{ fontSize: 55, cursor: 'pointer' }}>stop</Icon>
+                </div>
+                <div className="col-md-4">
+                    <Dropdown value={speedList} name='Speed' />
+                </div>
             </div>
-            <Icon color="primary" style={{ fontSize: 55,cursor:'pointer' }}>play_arrow</Icon>
-            <Icon color="primary" style={{ fontSize: 55,cursor:'pointer' }}>pause
-            </Icon>
-            <Icon color="primary" style={{ fontSize: 55,cursor:'pointer' }}>
-                stop
-            </Icon>
-            <button className="btn languagebtn btn-secondary dropdown-toggle languagedd" type="button" id="ddSpeedBtn" data-toggle="dropdown">
-                Speed
-            </button>
-            <div className="dropdown-menu" aria-labelledby="ddSpeedBtn">
-                <a className="dropdown-item" href="#">Low</a>
-                <a className="dropdown-item" href="#">Medium</a>
-                <a className="dropdown-item" href="#">High</a>
-            </div>
-        </div>
-    
-</div>
-
-    );
-  }
+        );
+    }
 }
 
 export default Player;
