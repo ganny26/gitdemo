e28a4e74fa0f798b7ae1ccf15f205b050e9da1ff
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-08 13:22:49 +0530

    app component updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavigationBar from "./components/NavigationBar";
 import MainContent from "./components/MainContent";
 import ProgressBar from "./components/ProgressBar";
+
 class App extends Component {
   render() {
     return (
