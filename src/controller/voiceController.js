5d8f12aba160155885a0ae3212143daf88c639cf
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-08 13:24:31 +0530

    controlled specific code added

---
+++ src/controller/voiceController.js
@@ -0,0 +1,13 @@
+let voicecontroller = {
+        getIngredients:function(){
+        console.log("getting ingredinets");
+    },
+    setLanguage:function(){
+        console.log("Setting Language");
+    },
+    setSpeed:function(){
+        console.log("Setting speed");
+    }
+}
+
+module.exports = voicecontroller;
