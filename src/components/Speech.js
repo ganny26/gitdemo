da8a93896fe94afeceddeb8ff3c3ef3447a1e291
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-09 00:30:09 +0530

    speech class added

---
+++ src/components/Speech.js
@@ -0,0 +1,19 @@
+class Speech{
+    constructor(voice, speed) {
+        this.voice = voice;
+        this.speed = speed;
+      }
+      
+    get getVoice(){
+        return this.voice;
+    }
+
+    get getSpeed(){
+        return this.speed;
+    }
+
+    static 
+
+}
+
+export default Speech;
