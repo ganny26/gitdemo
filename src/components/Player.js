33ac6a92f40ad001861ceae36584b3cb0c640c82
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-08 13:23:58 +0530

    player revamped

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import 'typeface-roboto';
 import Icon from 'material-ui/Icon';
-import IconManager from './IconManager';
+
 import Dropdown from './Dropdown';
+import AppConstants from './AppConstants';
 class Player extends Component {
     render() {
-        let languageList = ['English(IND)', 'English (US)', 'English(UK)'];
-        let speedList = ['Low', 'Medium', 'High'];
+        let languageList = AppConstants.languagelist;
+        let speedList = AppConstants.speedlist;
         return (
             <div className="row">
                 <div className="col-md-4">
-                    <Dropdown value={languageList} name='Select Language' />
+                    <Dropdown value={languageList} onClick="{}" purpose={AppConstants.language} name='Select Language' />
                 </div>
                 <div className="col-md-4 player-control">
                     <Icon color="primary" style={{ fontSize: 55, cursor: 'pointer' }}>play_arrow</Icon>
@@ -18,7 +19,7 @@ class Player extends Component {
                     <Icon color="primary" style={{ fontSize: 55, cursor: 'pointer' }}>stop</Icon>
                 </div>
                 <div className="col-md-4">
-                    <Dropdown value={speedList} name='Speed' />
+                    <Dropdown value={speedList}  purpose={AppConstants.speed} name='Speed' />
                 </div>
             </div>
         );
