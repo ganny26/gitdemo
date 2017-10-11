8d5a847d14457154bfd9aa9efe3c91f541026a25
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-11 22:01:38 +0530

    folder re architectured

---
+++ src/utils/AppConstants.js
@@ -0,0 +1,11 @@
+
+
+var AppConstants = {
+    "read":"READ",
+    "language":"LANGUAGE",
+    "speed":"SPEED",
+    "languagelist":['English(IND)', 'English (US)', 'English(UK)'],
+    "speedlist":['Low', 'Medium', 'High']
+}
+
+module.exports = AppConstants;
